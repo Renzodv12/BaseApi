56920b09de0169fc777b3e5a37738f753f4dcfbd
renzo.dragotto <renzo.dragotto@bancontinental.com.py>
2023-09-20 21:53:49 -0400

    first commit

---
+++ WebApi/Program.cs
@@ -0,0 +1,40 @@
+﻿using Serilog;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) => {
+    config.ReadFrom.Configuration(context.Configuration.GetSection("Serilog"));
+    config.Enrich.FromLogContext();
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "BaseApi", Version = "v1" });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseSerilogRequestLogging();
+
+app.MapControllers();
+
+app.Run();
