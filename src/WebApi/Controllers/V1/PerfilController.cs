cfc19c841c6b22a5efcebc37426dbbc2b92e6a65
unknown <renzodragotto50@gmail.com>
2024-01-23 20:46:26 -0300

    Perfil add

---
+++ src/WebApi/Controllers/V1/PerfilController.cs
@@ -0,0 +1,73 @@
+﻿using Core.Entities.Models.Perfil;
+using Infra.DBManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilController : BaseController
+    {
+        private readonly ApiDbContext _apiDbContext;
+    
+        public PerfilController(ApiDbContext apiDbContext)
+        {
+            _apiDbContext = apiDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Json(_apiDbContext.Perfil.ToList());
+
+        }
+
+        [HttpGet("{id}")]
+     public async Task<IActionResult> Get(int id)
+        {
+              return Json(await _apiDbContext.Perfil.Where(x => x.Id == id).FirstOrDefaultAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Perfil _perfil)
+        {
+            if(ModelState.IsValid)
+            {
+                await _apiDbContext.Perfil.AddAsync(_perfil);
+               await _apiDbContext.SaveChangesAsync();
+            }
+            return Json(_apiDbContext.Perfil.ToList());
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Perfil _perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                _apiDbContext.Update<Perfil>(_perfil);
+                await _apiDbContext.SaveChangesAsync();
+            }
+            return Json(_apiDbContext.Perfil.ToList());
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var _perfil = await _apiDbContext.Perfil.Where(x => x.Id == id).FirstOrDefaultAsync();
+               if(_perfil != null)
+                {
+
+                    _apiDbContext.Perfil.Remove(_perfil);
+                    await _apiDbContext.SaveChangesAsync();
+                }
+            }
+            return Json(_apiDbContext.Perfil.ToList());
+        }
+
+
+    }
+}
---
+++ src/WebApi/Migrations/20240123233106_Perfil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Perfil : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Perfil",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageSrc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Language = table.Column<int>(type: "int", nullable: false),
+                    FCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FModificacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perfil", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Perfil");
+        }
+    }
+}
