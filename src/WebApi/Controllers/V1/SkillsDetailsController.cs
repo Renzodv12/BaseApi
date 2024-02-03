385e1290eb42cf8405b5a9a3a94bc9a731702f10
renzo.dragotto <renzo.dragotto@bancontinental.com.py>
2024-02-02 23:48:07 -0300

    skillsdetails controller

---
+++ src/WebApi/Controllers/V1/SkillsDetailsController.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Models;
+using Infra.DBManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsDetailsController : BaseController<SkillsDetails>
+    {
+
+        public SkillsDetailsController(ApiDbContext apiDbContext) : base(apiDbContext)
+        {
+        }
+
+    }
+}
